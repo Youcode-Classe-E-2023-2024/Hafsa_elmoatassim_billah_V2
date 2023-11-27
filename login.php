eb40677c0c88d4ec3b721bd8d29ae46318ae5d97
HAFSA159 <hafsaelmoatassim99@gmail.com>
2023-11-27 15:03:49 +0100

    login

--- login.php
+++ login.php
@@ -6,70 +6,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login </title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <style>
-        body {
-            background-color: rgb(89, 89, 167);
-        }
-
-        .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        .form-container {
-            background-color: rgb(102, 102, 177);
-            padding: 20px;
-            border-radius: 10px;
-            width: 300px;
-        }
-
-        .form-container h1 {
-            color: #fff;
-            text-align: center;
-            margin-bottom: 20px;
-            font-size: x-large;
-        }
-
-        .form-input {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 10px;
-            border-radius: 5px;
-            border: none;
-        }
-
-        .form-button {
-            width: 100%;
-            padding: 10px;
-            border-radius: 5px;
-            border: none;
-            color: #fff;
-            background-color: rgb(89, 89, 167);
-            cursor: pointer;
-            margin-bottom: 10px;
-        }
-
-        .form-button:hover {
-            background-color: rgb(70, 70, 130);
-        }
-    </style>
+    
 </head>
-
 <body>
-    <div class="container">
+    
+
+<section class="bg-gray-50 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <div class="flex items-center justify-between">
+                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                  </div>
+                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <a href="signup.php" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
+</body>
+    <!-- <div class="container">
         <div class="form-container">
             <h1>Login</h1>
             <form action="#" method="post">
-                <input class="form-input" type="text" name="username" placeholder="Username" required>
+                <input type="email" name="email" required placeholder="enter your email" required>
                 <input class="form-input" type="password" name="password" placeholder="Password" required>
                 <button class="form-button" type="submit">Login</button>
                 <button class="form-button"><a href="./signup.php">Sign Up</a></button>
             </form>
         </div>
-    </div>
+    </div> -->
 
-</body>
+
 
 </html>
+
