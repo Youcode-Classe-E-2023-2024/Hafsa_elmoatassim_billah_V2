26b4923fb0f3e0d3d26c35697e28afb3eeb4c90d
HAFSA159 <hafsaelmoatassim99@gmail.com>
2023-11-28 21:44:29 +0100

    fix the last issue

--- add.php
+++ add.php
@@ -13,13 +13,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     }
 
     $sql = "INSERT INTO contact (title, description, price, category) VALUES ('$title', '$description', $price, '$category')";
+    $result = mysqli_query($conn,$sql);
+    // if ($conn->query($sql)) {
+    //     echo "Records added successfully.";
+    //     header("location: index.php");
+    // } else {
+    //     echo "Error: " . $sql . "<br>" . $conn->error;
+    // }
 
-    if ($conn->query($sql)) {
-        echo "Records added successfully.";
-        header("location: index.php");
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+    header("Location: annonce.php");
 }
 
 // Close the connection
